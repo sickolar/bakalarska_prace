39a172460712ccf4d8221b073378652eb4872ee2
sickolar <andrejicek.kolar@gmail.com>
2025-11-08 21:15:43 +0100

    Prepsano, aby pouze nacitala

--- Nacteni.py
+++ Nacteni.py
@@ -1,11 +1,9 @@
 import pydicom
-import matplotlib.pyplot as plt
 import numpy as np
 import os
-from matplotlib.widgets import Slider
 
 #Nacteni souboru, vythoreni 3D pole (rez, vyska, sirka)
-def data_load(path):
+def data_load(path, return_metada = False):
     dcm_files = []
     data = []
 
@@ -13,8 +11,6 @@ def data_load(path):
         if file.endswith('.dcm'):
             dcm = os.path.join(path, file)
             dcm_files.append(dcm)
-
-    print(dcm_files)
 
     if dcm_files == []: #osetreni - nenalezeni souboru
         print('Neni nalezen soubor')
@@ -29,34 +25,10 @@ def data_load(path):
 
     data = np.array(data) #vlozeni do numpy pole
 
-    number_of_images = len(data)#pocet nactenych obrazku
+    number_of_images = len(data)#pocet nactenych rezu
+    print(f'Pocet nactenych rezu: {number_of_images}')
+    return data
 
-
-    fig, ax = plt.subplots() #fig - cele okno, ax - plocha pro vykresleni
-    plt.subplots_adjust(bottom=0.2) #pro slider
-    image_displayed = ax.imshow(data[0], cmap='grey') #imshow, pro prvni obrazek, ax konkretni osa
-    ax.set_title(f'Snimek 1 z {number_of_images}')
-
-    #Slider
-    slider_position = plt.axes([0.2, 0.05, 0.6, 0.03]) #Osa pro slider - kde se bude nachazet v ramci fig
-    slider = Slider(slider_position, label='Snímek', valmin=0, valmax=number_of_images - 1, valinit=0, valstep=1)#nastaveni slideru
-
-    slider.on_changed(lambda val: slider_update(slider, data, fig, ax, image_displayed, number_of_images)) #lambad premostovaci funkce - prijme value, preda ji slider update
-    plt.show() #zobrazi otevrenou fig
-#Funkce pro posun slideru
-def slider_update(slider, data, fig, ax, image_displayed, number_of_images):
-    i = int(slider.val) #nacte, kde se nachazime na slideru - rez
-    image_displayed.set_data(data[i]) #zobrazi snimek podle pozice slideru
-    ax.set_title(f'Snímek {i + 1} z {number_of_images}') #zmeni popisek
-    fig.canvas.draw_idle() #aktualizujeme zobrazeny obrazek.
-
-
-
-
-
-
-
-data_load(r'C:\Users\andre\OneDrive\Plocha\School\BTB_3\Bakalářská práce\subjekty\pokus_1\pacient_1')
 
 
 
